0683f073d631693356e061ba2ed629f1fdab1230
Hayse3101 <lolartem999@gmail.com>
2024-05-24 15:45:08 +0300

    commit and push 24.05.2024 - 15:45

--- Django_project/django_movie/movies/views.py
+++ Django_project/django_movie/movies/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import ListView, DetailView
+from .models import Movie
 
-# Create your views here.
+
+class MoviesView(ListView):
+    """ Список фильмов """
+    model = Movie
+    queryset = Movie.objects.filter(draft=False)
+
+
+class MovieDetailView(View):
+    """ Полное описание фильма """
+    model = Movie
+    slug_field = "url"
