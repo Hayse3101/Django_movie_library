c55e591370547302a30f4a9d5663858e7c4ef26f
Hayse3101 <lolartem999@gmail.com>
2024-05-31 22:10:59 +0300

    commit and push 31.05.2024 - 22:10

--- Django_project/django_movie/movies/admin.py
+++ Django_project/django_movie/movies/admin.py
@@ -1,7 +1,18 @@
+from django import forms
 from django.contrib import admin
 from django.utils.safestring import mark_safe
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
 
-from .models import Category, Genre, Movie, MovieShots, Actor, Rating, RatingStar, Reviews
+from .models import Category, Genre, Movie, MovieShorts, Actor, Rating, RatingStar, Reviews
+
+
+class MovieAdminForm(forms.ModelForm):
+    """Форма с виджетом ckeditor"""
+    description = forms.CharField(label="Описание", widget=CKEditorUploadingWidget())
+
+    class Meta:
+        model = Movie
+        fields = '__all__'
 
 
 @admin.register(Category)
@@ -19,7 +30,7 @@ class ReviewInline(admin.TabularInline):
 
 
 class MovieShotsInline(admin.TabularInline):
-    model = MovieShots
+    model = MovieShorts
     extra = 1
     readonly_fields = ("get_image",)
 
@@ -39,6 +50,8 @@ class MovieAdmin(admin.ModelAdmin):
     save_on_top = True
     save_as = True
     list_editable = ("draft",)
+    actions = ["publish", "unpublish"]
+    form = MovieAdminForm
     readonly_fields = ("get_image",)
     fieldsets = (
         (None, {
@@ -65,12 +78,36 @@ class MovieAdmin(admin.ModelAdmin):
     def get_image(self, obj):
         return mark_safe(f'<img src={obj.poster.url} width="100" height="110"')
 
+    def unpublish(self, request, queryset):
+        """Снять с публикации"""
+        row_update = queryset.update(draft=True)
+        if row_update == 1:
+            message_bit = "1 запись была обновлена"
+        else:
+            message_bit = f"{row_update} записей были обновлены"
+        self.message_user(request, f"{message_bit}")
+
+    def publish(self, request, queryset):
+        """Опубликовать"""
+        row_update = queryset.update(draft=False)
+        if row_update == 1:
+            message_bit = "1 запись была обновлена"
+        else:
+            message_bit = f"{row_update} записей были обновлены"
+        self.message_user(request, f"{message_bit}")
+
+    publish.short_description = "Опубликовать"
+    publish.allowed_permissions = ('change', )
+
+    unpublish.short_description = "Снять с публикации"
+    unpublish.allowed_permissions = ('change',)
+
     get_image.short_description = "Постер"
 
 
 @admin.register(Reviews)
 class ReviewAdmin(admin.ModelAdmin):
-    """Отзывы"""
+    """Отзывы к фильму"""
     list_display = ("name", "email", "parent", "movie", "id")
     readonly_fields = ("name", "email")
 
@@ -99,7 +136,7 @@ class RatingAdmin(admin.ModelAdmin):
     list_display = ("star", "ip")
 
 
-@admin.register(MovieShots)
+@admin.register(MovieShorts)
 class MovieShotsAdmin(admin.ModelAdmin):
     """Кадры из фильма"""
     list_display = ("title", "movie", "get_image")
--- Django_project/django_movie/movies/views.py
+++ Django_project/django_movie/movies/views.py
@@ -1,31 +1,32 @@
 from django.shortcuts import render, redirect
-from django.views import View
 from django.views.generic import ListView, DetailView
-from .models import Movie
+from django.views.generic.base import View
+
+from .models import Movie, Category
 from .forms import ReviewForm
 
 
 class MoviesView(ListView):
-    """ Список фильмов """
+    """Список фильмов"""
     model = Movie
     queryset = Movie.objects.filter(draft=False)
 
 
-class MovieDetailView(View):
-    """ Полное описание фильма """
+class MovieDetailView(DetailView):
+    """Полное описание фильма"""
     model = Movie
     slug_field = "url"
 
 
 class AddReview(View):
-    """ Отзывы """
+    """Отзывы"""
     def post(self, request, pk):
         form = ReviewForm(request.POST)
         movie = Movie.objects.get(id=pk)
         if form.is_valid():
             form = form.save(commit=False)
-            if request.POST.get('parent', None):
-                form.parent_id = int(request.POST.get('parent'))
+            if request.POST.get("parent", None):
+                form.parent_id = int(request.POST.get("parent"))
             form.movie = movie
             form.save()
         return redirect(movie.get_absolute_url())
