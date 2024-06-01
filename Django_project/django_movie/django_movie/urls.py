dfb6eae28c0d22a2b481525e1713ed44b5753275
Hayse3101 <lolartem999@gmail.com>
2024-06-01 21:49:06 +0300

    commit and push 01.06.2024 - 21:49

--- Django_project/django_movie/django_movie/urls.py
+++ Django_project/django_movie/django_movie/urls.py
@@ -1,29 +1,14 @@
+from django.urls import path
 
-"""django_movie URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib import admin
-from django.urls import path, include
+from movies import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('ckeditor/', include('ckeditor_uploader.urls')),
-    path("", include("movies.urls")),
+    path("", views.MoviesView.as_view()),
+    path("filter/", views.FilterMoviesView.as_view(), name='filter'),
+    path("search/", views.Search.as_view(), name='search'),
+    path("add-rating/", views.AddStarRating.as_view(), name='add_rating'),
+    path("json-filter/", views.JsonFilterMoviesView.as_view(), name='json_filter'),
+    path("<slug:slug>/", views.MovieDetailView.as_view(), name="movie_detail"),
+    path("review/<int:pk>/", views.AddReview.as_view(), name="add_review"),
+    path("actor/<str:slug>/", views.ActorView.as_view(), name="actor_detail"),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
